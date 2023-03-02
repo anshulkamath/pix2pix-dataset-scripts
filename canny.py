ad64fc36ac18e986a20eb6988d9870b391979af0
Anshul Kamath <anshulkam@gmail.com>
2023-03-02 13:25:10 -0800

    initial commit: added all scripting files

---
+++ canny.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import argparse
+import cv2
+from matplotlib import pyplot as plt
+from PIL import Image
+import re
+
+parser = argparse.ArgumentParser(prog='EdgeDetector', description='Takes in images and runs edge detection on them')
+
+parser.add_argument('filename')
+parser.add_argument('-o', '--output')
+parser.add_argument('-s', '--show', action='store_true')
+parser.add_argument('-h', '--height', default=128)
+args = parser.parse_args()
+
+img = cv2.cvtColor(cv2.imread(args.filename), cv2.COLOR_BGR2RGB)
+edges = cv2.bitwise_not(cv2.Canny(img, 50, 200))
+
+SIDE_DIMENSION = args.height
+
+if args.show:
+  plt.subplot(121),plt.imshow(img,cmap = 'gray')
+  plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+  plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+  plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+  plt.show()
+
+if args.output:
+  img = Image.fromarray(img)
+  edges = Image.fromarray(edges)
+  width, height = img.size
+
+  prefix, extension = re.split(r'\.', args.output)
+
+  if height != SIDE_DIMENSION:
+    raise Exception('The images should have a height of 128 pixels')
+  
+  if width == SIDE_DIMENSION:
+    new_image = Image.new('RGB', (2 * height, height))
+    new_image.paste(edges, (0, 0))
+    new_image.paste(img, (SIDE_DIMENSION, 0))
+    new_image.save(f'{prefix}a.{extension}')
+    exit(0)
+
+  new_images = [Image.new('RGB', (2 * height, height)) for _ in range(2)]
+
+  left_crop = (
+    *(im.crop((0, 0, SIDE_DIMENSION, height)) for im in (edges, img)),
+    'a'
+  )
+
+  right_crop = (
+    *(im.crop((width - SIDE_DIMENSION, 0, width, height)) for im in (edges, img)),
+    'b'
+  )
+
+  for new_im in new_images:
+    for (edges, im, suffix) in (left_crop, right_crop):
+      new_im.paste(edges, (0, 0))
+      new_im.paste(im, (SIDE_DIMENSION, 0))
+      new_im.save(f'{prefix}{suffix}.{extension}')
---
+++ generate-dataset.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+INPUT_DIR='input'
+VIDEO_DIR='videos'
+OUTPUT_DIR='output'
+DATASET_NAME='pix2pix-dataset'
+DIRECTORIES=()
+OVERWRITE=false
+
+function make_directories() {
+  # Create necessary input directories
+  if [[ ! -d $INPUT_DIR ]]; then
+    mkdir $INPUT_DIR
+  fi
+
+  for i in $(ls $VIDEO_DIR); do
+    curr="${i%.mp4}"
+
+    DIRECTORIES+=("$curr")
+    DIRECTORY_PATH="$INPUT_DIR/$curr"
+
+    if ! $OVERWRITE; then
+      continue
+    fi
+
+    if [[ -d $DIRECTORY_PATH ]]; then
+      rm -rf $DIRECTORY_PATH
+    fi
+
+    mkdir $DIRECTORY_PATH
+  done
+}
+
+function extract_photos() {
+  # Extract the images and downsize them
+  HIGH_SAMPLE='1'
+  LOW_SAMPLE='1/3'
+  for i in "${DIRECTORIES[@]}"; do
+    IMG_HEIGHT='128'
+    FFMPEG_OUTPUT_PREFIX="$INPUT_DIR/$i/$i-"
+
+    if [[ $i == 'ny-skyline' ]]; then
+      ffmpeg -i "$VIDEO_DIR/$i.mp4" -vf "fps=$LOW_SAMPLE,scale=-1:$IMG_HEIGHT" -q:v 2 "${FFMPEG_OUTPUT_PREFIX}%03d.jpg"
+      continue
+    fi
+
+    ffmpeg -i "$VIDEO_DIR/$i.mp4" -vf "fps=$HIGH_SAMPLE,scale=-1:$IMG_HEIGHT" -q:v 2 "${FFMPEG_OUTPUT_PREFIX}%03d.jpg"
+  done
+}
+
+function create_edges() {
+  # Convert each image to canny counterpart
+  for i in "${DIRECTORIES[@]}"; do
+    FILE_DIR="$INPUT_DIR/$i"
+
+    for j in $(ls "$INPUT_DIR/$i"); do
+      FILE="$INPUT_DIR/$i/$j"
+      ./canny.py "$FILE" -o "$FILE"
+      rm $FILE
+    done
+  done
+}
+
+function create_dataset() {
+  splitfolders --output "$OUTPUT_DIR" --ratio .8 .1 .1 -- $INPUT_DIR
+  for i in $(ls $OUTPUT_DIR); do
+    for j in "${DIRECTORIES[@]}"; do
+      mv $OUTPUT_DIR/$i/$j/* $OUTPUT_DIR/$i
+      rmdir $OUTPUT_DIR/$i/$j
+    done
+  done
+
+  zip -r $DATASET_NAME $OUTPUT_DIR >> /dev/null
+}
+
+make_directories
+if $OVERWRITE ; then
+  extract_photos
+  create_edges
+fi
+create_dataset
